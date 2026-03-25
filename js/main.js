df2463c8d62ec2e6c67054b07373623885c9e4c1
AK1798 <kallelmir21@gmail.com>
2026-03-25 01:37:24 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Exemple simple : scroll smooth pour le menu
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
